da65a2384e47fd8bdb2ed199aa3cd59eeb1a1019
s-beats <sakashin10291029@gmail.com>
2022-11-06 14:38:47 +0900

    implement pervious permuutation

--- abc276/c/main.go
+++ abc276/c/main.go
@@ -42,42 +42,50 @@ func toInt(s string) int {
 	return n
 }
 
-func main() {
-	N := toInt(readline())
-	P := readIntSlice()
+func perviousPermuutation(src []int) []int {
+	size := len(src)
+	dst := make([]int, size)
+	copy(dst, src)
 
 	// 入力の境目(大きい範囲の末尾)を探す
 	boaderIdx := 0
 	boader := -1
-	for i := N - 1; i > 0; i-- {
-		if P[i-1] > P[i] {
+	for i := size - 1; i > 0; i-- {
+		if src[i-1] > src[i] {
 			boaderIdx = i - 1
-			boader = P[i-1]
+			boader = src[i-1]
 			break
 		}
 	}
 
 	// 境目以降を降順
-	sort.Slice(P[boaderIdx:], func(i, j int) bool {
-		return P[i+boaderIdx] > P[j+boaderIdx]
+	sort.Slice(dst[boaderIdx:], func(i, j int) bool {
+		return dst[i+boaderIdx] > dst[j+boaderIdx]
 	})
 
 	// 境目よりひとつ小さい値を探す
-	for i := boaderIdx; i < N; i++ {
-		if P[i] < boader {
-			P[boaderIdx], P[i] = P[i], P[boaderIdx]
+	for i := boaderIdx; i < size; i++ {
+		if dst[i] < boader {
+			dst[boaderIdx], dst[i] = dst[i], dst[boaderIdx]
 			break
 		}
 	}
 
 	// 境目+1以降を降順
-	sort.Slice(P[boaderIdx+1:], func(i, j int) bool {
-		return P[i+boaderIdx+1] > P[j+boaderIdx+1]
+	sort.Slice(dst[boaderIdx+1:], func(i, j int) bool {
+		return dst[i+boaderIdx+1] > dst[j+boaderIdx+1]
 	})
 
+	return dst
+}
+
+func main() {
+	_ = toInt(readline())
+	P := readIntSlice()
+
 	var ans string
-	for i := 0; i < N; i++ {
-		ans += strconv.Itoa(P[i]) + " "
+	for _, v := range perviousPermuutation(P) {
+		ans += strconv.Itoa(v) + " "
 	}
 	Println(strings.TrimRight(ans, " "))
 }
--- template/util.go
+++ template/util.go
@@ -1,6 +1,9 @@
 package main
 
-import "math"
+import (
+	"math"
+	"sort"
+)
 
 func mod(n1, n2 int) int {
 	res := (n1 + n2) % n2
@@ -30,4 +33,41 @@ func isOdd(n int) bool {
 
 func isEven(n int) bool {
 	return n&1 == 0
+}
+
+func perviousPermuutation(src []int) []int {
+	size := len(src)
+	dst := make([]int, size)
+	copy(dst, src)
+
+	// 入力の境目(大きい範囲の末尾)を探す
+	boaderIdx := 0
+	boader := -1
+	for i := size - 1; i > 0; i-- {
+		if src[i-1] > src[i] {
+			boaderIdx = i - 1
+			boader = src[i-1]
+			break
+		}
+	}
+
+	// 境目以降を降順
+	sort.Slice(dst[boaderIdx:], func(i, j int) bool {
+		return dst[i+boaderIdx] > dst[j+boaderIdx]
+	})
+
+	// 境目よりひとつ小さい値を探す
+	for i := boaderIdx; i < size; i++ {
+		if dst[i] < boader {
+			dst[boaderIdx], dst[i] = dst[i], dst[boaderIdx]
+			break
+		}
+	}
+
+	// 境目+1以降を降順
+	sort.Slice(dst[boaderIdx+1:], func(i, j int) bool {
+		return dst[i+boaderIdx+1] > dst[j+boaderIdx+1]
+	})
+
+	return dst
 }
