3a39d6df56f0c3e862d6f3b8c32f2e7d4cd461cd
s-beats <sakashin10291029@gmail.com>
2022-12-27 10:32:10 +0900

    第１２回日本情報オリンピック 予選（過去問）/Dの実装

---
+++ joi2013yo/d/main.go
@@ -0,0 +1,170 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+// 1行の文字列がデフォルトを超えることがあるのでサイズを明示的に指定
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+
+func readline() string {
+	buf := make([]byte, 0, 16)
+	for {
+		l, p, err := rdr.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func readIntSlice() []int {
+	slice := make([]int, 0)
+	lines := strings.Split(readline(), " ")
+	for _, v := range lines {
+		slice = append(slice, toInt(v))
+	}
+	return slice
+}
+
+func readIntSlice2() (a, b int) {
+	slice := make([]int, 0)
+	lines := strings.Split(readline(), " ")
+	for _, v := range lines {
+		slice = append(slice, toInt(v))
+	}
+	return slice[0], slice[1]
+}
+
+func readIntSlice3() (a, b, c int) {
+	slice := make([]int, 0)
+	lines := strings.Split(readline(), " ")
+	for _, v := range lines {
+		slice = append(slice, toInt(v))
+	}
+	return slice[0], slice[1], slice[2]
+}
+
+func toInt(s string) int {
+	n, _ := strconv.Atoi(s)
+	return n
+}
+
+func toStr(n int) string {
+	return strconv.Itoa(n)
+}
+
+func isInt(s string) bool {
+	_, err := strconv.Atoi(s)
+	return err == nil
+}
+
+func abs(n int) int {
+	return int(math.Abs(float64(n)))
+}
+
+func max(x, y int) int {
+	return int(math.Max(float64(x), float64(y)))
+}
+
+func min(x, y int) int {
+	return int(math.Min(float64(x), float64(y)))
+}
+
+func isOdd(n int) bool {
+	return n&1 == 1
+}
+
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// 1文字
+func isUpper(s string) bool {
+	return unicode.IsUpper(rune(s[0]))
+}
+
+// 1文字
+func isLower(s string) bool {
+	return unicode.IsUpper(rune(s[0]))
+}
+
+var (
+	dr = []int{1, -1, 0, 0}
+	dc = []int{0, 0, 1, -1}
+)
+
+func main() {
+	d, n := readIntSlice2()
+	ts := make([]int, d+1)
+	for i := 1; i < d+1; i++ {
+		ts[i] = toInt(readline())
+	}
+	abcs := make([][]int, n+1)
+	for i := 1; i < n+1; i++ {
+		abcs[i] = readIntSlice()
+	}
+
+	// i日目にjを選んだ場合の最大値
+	dp := make([][]int, d+1)
+	for i := 1; i < d+1; i++ {
+		dp[i] = make([]int, n+1)
+		for j := range dp[i] {
+			dp[i][j] = -1
+		}
+	}
+
+	// i日目にjが着られるかどうか
+	check := func(i int, j int) bool {
+		return abcs[j][0] <= ts[i] && ts[i] <= abcs[j][1]
+	}
+
+	// 1日目は0で埋めておく
+	for j := 1; j < n+1; j++ {
+		if check(1, j) {
+			dp[1][j] = 0
+		}
+	}
+
+	for i := 1; i < d; i++ {
+		for j := 1; j < n+1; j++ {
+			if dp[i][j] == -1 {
+				continue
+			}
+			for k := 1; k < n+1; k++ {
+				if !check(i+1, k) {
+					continue
+				}
+				delta := abs(abcs[j][2] - abcs[k][2])
+				if i > 1 {
+					delta += dp[i][j]
+				}
+				dp[i+1][k] = max(dp[i+1][k], delta)
+			}
+		}
+	}
+
+	ans := 0
+	for _, v := range dp[d] {
+		ans = max(ans, v)
+	}
+	Println(ans)
+}
