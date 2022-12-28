211be91abec4bdaed772c71e484a15e6f325caed
s-beats <sakashin10291029@gmail.com>
2022-12-28 12:18:22 +0900

    第１４回日本情報オリンピック 予選（過去問）/Dの実装

---
+++ joi2015yo/d/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"bufio"
+	. "fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+// 1行の文字列がデフォルトを超えることがあるのでサイズを明示的に指定
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+
+func readline() string {
+	buf := make([]byte, 0, 16)
+	for {
+		l, p, err := rdr.ReadLine()
+		if err != nil {
+			panic(err)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func readIntSlice() []int {
+	slice := make([]int, 0)
+	lines := strings.Split(readline(), " ")
+	for _, v := range lines {
+		slice = append(slice, toInt(v))
+	}
+	return slice
+}
+
+func readIntSlice2() (a, b int) {
+	slice := make([]int, 0)
+	lines := strings.Split(readline(), " ")
+	for _, v := range lines {
+		slice = append(slice, toInt(v))
+	}
+	return slice[0], slice[1]
+}
+
+func readIntSlice3() (a, b, c int) {
+	slice := make([]int, 0)
+	lines := strings.Split(readline(), " ")
+	for _, v := range lines {
+		slice = append(slice, toInt(v))
+	}
+	return slice[0], slice[1], slice[2]
+}
+
+func toInt(s string) int {
+	n, _ := strconv.Atoi(s)
+	return n
+}
+
+func toStr(n int) string {
+	return strconv.Itoa(n)
+}
+
+func isInt(s string) bool {
+	_, err := strconv.Atoi(s)
+	return err == nil
+}
+
+func abs(n int) int {
+	return int(math.Abs(float64(n)))
+}
+
+func max(x, y int) int {
+	return int(math.Max(float64(x), float64(y)))
+}
+
+func min(x, y int) int {
+	return int(math.Min(float64(x), float64(y)))
+}
+
+func isOdd(n int) bool {
+	return n&1 == 1
+}
+
+func isPrime(n int) bool {
+	if n < 2 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+// 1文字
+func isUpper(s string) bool {
+	return unicode.IsUpper(rune(s[0]))
+}
+
+// 1文字
+func isLower(s string) bool {
+	return unicode.IsUpper(rune(s[0]))
+}
+
+var (
+	dr = []int{1, -1, 0, 0}
+	dc = []int{0, 0, 1, -1}
+)
+
+func main() {
+	n, m := readIntSlice2()
+	d := make([]int, n+1)
+	for i := 1; i < n+1; i++ {
+		d[i] = toInt(readline())
+	}
+	c := make([]int, m+1)
+	for i := 1; i < m+1; i++ {
+		c[i] = toInt(readline())
+	}
+
+	dp := make([][]int, n+1)
+	for i := 1; i < n+1; i++ {
+		dp[i] = make([]int, m+1)
+		for j := range dp[i] {
+			dp[i][j] = math.MaxInt64
+		}
+	}
+
+	for i := 1; i < m+1; i++ {
+		for j := 1; j < m+1; j++ {
+			dp[1][j] = min(dp[1][j], d[1]*c[j])
+		}
+	}
+
+	for i := 1; i < n; i++ {
+		for j := i; j < m+1; j++ {
+			for k := j + 1; k < m+1; k++ {
+				dp[i+1][k] = min(dp[i+1][k], (d[i+1]*c[k])+dp[i][j])
+			}
+		}
+	}
+
+	ans := dp[n][n]
+	for _, v := range dp[n][n+1:] {
+		ans = min(ans, v)
+	}
+	Println(ans)
+}
